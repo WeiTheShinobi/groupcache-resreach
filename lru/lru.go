f4653198ab44f462f87e7d206c8426d052045cb5
WeiTheShinobi <weitheshinobi@gmail.com>
2022-12-02 11:53:10 +0800

    RemoveOldest()

--- lru/lru.go
+++ lru/lru.go
@@ -13,7 +13,7 @@ LRU認為最近被查找過，
 type Cache struct {
 	maxBytes  int64
 	nBytes    int64
-	m         map[string]*list.Element
+	cache     map[string]*list.Element
 	ll        *list.List
 	onEvicted func(key string, value Value)
 }
@@ -28,16 +28,26 @@ type Value interface {
 }
 
 func (c *Cache) Get(key string) (Value, bool) {
-	if v, ok := c.m[key]; ok {
-		c.ll.MoveToFront(v)
-		en := v.Value.(*entry)
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		en := ele.Value.(*entry)
 		return en.value, true
 	}
 	return nil, false
 }
 
 func (c *Cache) RemoveOldest() {
+	ele := c.ll.Back()
+	if ele != nil {
+		c.ll.Remove(ele)
 
+		en := ele.Value.(*entry)
+		delete(c.cache, en.key)
+		c.nBytes -= int64(len(en.key)) + int64(en.value.Len())
+		if c.onEvicted != nil {
+			c.onEvicted(en.key, en.value)
+		}
+	}
 }
 
 func (c *Cache) Add(key string, value Value) {
